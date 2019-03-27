dba117d5736c746bf420e27bfb4b59e4e63e097f
visualstudio <visualstudio@122-PC12>
2019-03-27 10:18:03 -0400

    Commit10:20

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Download();
+        }
+
+        static void Download()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("Complete");
+            });
+    }
+    }
+}
